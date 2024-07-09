aa98344ba2f270e3f3d49cfa2195feef64c93df3
Dipshandc <dipshandcnew@gmail.com>
2024-07-09 20:33:34 +0545

    middleware updated

--- auth/middleware.py
+++ auth/middleware.py
@@ -1,31 +1,20 @@
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+from rest_framework.authentication import get_authorization_header
+from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import AnonymousUser
-from channels.middleware.base import BaseMiddleware
-from channels.db import database_sync_to_async
-from rest_framework_simplejwt.tokens import UntypedToken
-from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
-from django.contrib.auth import get_user_model
-from jwt import decode as jwt_decode
-from django.conf import settings
 
-@database_sync_to_async
-def get_user(validated_token):
-    try:
-        user_model = get_user_model()
-        user_id = jwt_decode(validated_token, settings.SECRET_KEY, algorithms=["HS256"])["user_id"]
-        return user_model.objects.get(id=user_id)
-    except Exception as e:
-        return AnonymousUser()
+class BearerTokenMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        auth_header = get_authorization_header(request).split()
 
-class TokenAuthMiddleware(BaseMiddleware):
-    async def __call__(self, scope, receive, send):
-        headers = dict(scope["headers"])
-        if b"authorization" in headers:
-            token_name, token_key = headers[b"authorization"].decode().split()
+        if auth_header and auth_header[0].lower() == b'bearer':
             try:
-                UntypedToken(token_key)
-                scope["user"] = await get_user(token_key)
-            except (InvalidToken, TokenError):
-                scope["user"] = AnonymousUser()
-        else:
-            scope["user"] = AnonymousUser()
-        return await super().__call__(scope, receive, send)
+                token_key = auth_header[1].decode('utf-8')
+                token = Token.objects.get(key=token_key)
+                request.user = token.user
+            except (Token.DoesNotExist, UnicodeError, IndexError):
+                request.user = AnonymousUser()
+
+    def process_response(self, request, response):
+        return response
