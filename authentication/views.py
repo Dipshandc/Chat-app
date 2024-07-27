6d7fca65a080f739d7f9b60ba07e650836ddcb30
Dipshandc <dipshandcnew@gmail.com>
2024-07-27 16:18:12 +0545

    fix:error message

--- authentication/views.py
+++ authentication/views.py
@@ -30,12 +30,10 @@ class UserViewSet(APIView):
     )
     def post(self, request, *args, **kwargs):
             serializer = self.serializer_class(data=request.data)
-            if serializer.is_valid(raise_exception=True):
-                self.perform_create(serializer)
-                headers = self.get_success_headers(serializer.data)
-                return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED, headers=headers)
-            else:
-                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            serializer.is_valid(raise_exception=True)
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED, headers=headers)
                 
     def perform_create(self, serializer):
         serializer.save()
