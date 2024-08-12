2df813aa23c2c0d5cebf71a7b983ca56693c23d1
Dipshandc <dipshandcnew@gmail.com>
2024-08-12 18:04:50 +0545

    Update:Cloud storage for media in prod

--- chat/settings/dev.py
+++ chat/settings/dev.py
@@ -1,4 +1,7 @@
 from .common import *
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
 
 DEBUG = True
 
@@ -17,4 +20,19 @@ CHANNEL_LAYERS = {
             "hosts": [("127.0.0.1", 6379)],
         },
     },
+}
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': env('CLOUD_NAME'),
+    'API_KEY': env('API_KEY'),
+    'API_SECRET': env('API_SECRET'),
+}
+
+# Configure Django to use Cloudinary for media files
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
+CLOUDINARY = {
+    'secure': True,
 }
